da52b6e61976b0a7917d94115e56771c67af661a
nchenyp <chen.nicolas@hotmail.com>
2024-07-05 10:23:46 +0100

    Fixed argparse

--- TraT_sim_one_donor.py
+++ TraT_sim_one_donor.py
@@ -8,17 +8,59 @@ from numba import jit, prange
 import itertools
 import scipy.stats as st
 import pickle as pkl
-import argparse
+import argparse, os
 
 parser = argparse.ArgumentParser(description="Simulates the spreading of a single plasmid depending on the exclusion index (EI)")
-parser.add_argument("-m", "--mode", default='m', help="'m' (movie) or 'b' (batch). Default = 'm'")
-parser.add_argument("-ei", "--exclusion-index", default=1, help="Exclusion index. Default = 1 (no exclusion)")
-parser.add_argument("-r", "--repeat", default=50, help="Number of repeats in batch mode. Default = 50")
-parser.add_argument("-d", "--dead-cutoff", default=5, help="Dead cutoff. Minimum number of simultaneous mating into the same recipient which would result in the death of the recipient (lethal zygosis). Default = 5")
+parser.add_argument("-m", "--mode", default='movie', type=str, help="Either 'movie'/'m' or 'batch'/'b'. Default = 'movie'")
+parser.add_argument("-ei", "--exclusion-index", default=1, type=int, help="Exclusion index in movie mode. Default = 1 (no exclusion).")
+parser.add_argument("-eis", "--exclusion-indices", default=[1, 5, 200], nargs='+', type=int, help="Exclusion indices in batch mode, enter multiple values separated by a space. Default = 1 5 200")
+parser.add_argument("-r", "--repeat", default=50, type=int, help="Number of repeats in batch mode. Default = 50")
+parser.add_argument("-d", "--dead-cutoff", default=5, type=int, help="Dead cutoff. Minimum number of simultaneous mating into the same recipient which would result in the death of the recipient (lethal zygosis). Default = 5")
+parser.add_argument("-s", "--frame-size", default=100, type=int, help="Size of the canvas. Default = 100 (100x100 pixels)")
+parser.add_argument("-l", "--sim-len", default=500, type=int, help="Simulation length (number of time steps). Default = 500")
+parser.add_argument("-o", "--output", default='', type=str, help="Output directory. If a valid path is set, saves the movie ('movie' mode - mp4) or plots ('batch' mode - svg) to the set path.")
 args = parser.parse_args()
 
+# Check validity of inputs
+if args.mode not in ('movie', 'm', 'batch', 'b'):
+    raise ValueError("Mode must be either 'movie' or 'batch'")
+
+if args.exclusion_index < 1 :
+    raise ValueError("Exclusion index must be greater than or equal to 1")
+
+if type(args.exclusion_indices) is int :
+    if args.exclusion_indices < 1:
+        raise ValueError("Exclusion index must be greater than or equal to 1")
+    else :
+        args.exclusion_indices = [args.exclusion_indices]
+else :
+    # Check it's an iterable (list)
+    try :
+        len(args.exclusion_indices)
+    except :
+        raise TypeError("Wrong format for exclusion indices")
+    for ei in args.exclusion_indices :
+        if ei < 1:
+            raise ValueError("Exclusion index must be greater than or equal to 1")
+
+if args.repeat <= 0:
+    raise ValueError("Number of repeats must be greater than zero")
+
+if args.dead_cutoff <= 0:
+    raise ValueError("Dead cutoff must be greater than zero")
+
+if args.frame_size < 1:
+    raise ValueError("Frame size must be greater or equal to 1")
+
+if args.sim_len < 1:
+    raise ValueError("Simulation length must be greater or equal to 1")
+
+if args.output.replace(' ', '') == '':
+    print('No output path specified. Output will not be saved.')
+elif not os.path.isdir(args.output) :
+    raise TypeError("Output path does not exist")
+
 mpl.rcParams['font.family'] = 'Helvetica Neue'
-plot_animation = True
 
 # Generate list of all possible combinations of surroundding pixels order
 base_surround_coords = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
@@ -29,17 +71,11 @@ RECIPIENT = 0
 DONOR1 = 1
 DEAD = 100
 
-# How many matings in one step to kill the recipient? (1-8, if set >8 then no death by overmating)
-DEAD_CUTOFF = 5
+DEAD_CUTOFF = args.dead_cutoff
 
-cell_type = {
-    'recipient': 0,
-    'donor1' : 1,
-    'dead' : 100
-}
 
 @jit(nopython=True, parallel=True)
-def update_frames(N, current_frame, intermediate_frame_donor1, sfx1, base_mating_success_rate_donor1):
+def update_frames(N, current_frame, intermediate_frame_donor1, EI1, base_mating_success_rate_donor1):
     for x in prange(N):
         for y in range(N):
             if current_frame[x, y] in [RECIPIENT, DEAD]:
@@ -58,7 +94,7 @@ def update_frames(N, current_frame, intermediate_frame_donor1, sfx1, base_mating
                 if current_frame[ni, nj] == RECIPIENT and np.random.random() < base_mating_success_rate_donor1:
                     intermediate_frame_donor1[ni, nj] += 1
                     break
-                if current_frame[ni, nj] == DONOR1 and np.random.random() < (base_mating_success_rate_donor1 / sfx1):
+                if current_frame[ni, nj] == DONOR1 and np.random.random() < (base_mating_success_rate_donor1 / EI1):
                     intermediate_frame_donor1[ni, nj] += 1
                     break
 
@@ -79,8 +115,10 @@ def apply_updates(N, current_frame, intermediate_frame_donor1, dead_cutoff):
                 next_frame[x, y] = DONOR1
     return next_frame
 
-def simulate_single_repeat(k, sfx1, N, max_steps, base_mating_success_rate_donor1, return_full_movie, dead_cutoff):
-    print(f'Repeat : {k+1}')
+def simulate_single_repeat(k, EI1, N, max_steps, base_mating_success_rate_donor1, return_full_movie, dead_cutoff, print_repeat):
+    if print_repeat :
+        print(f'Repeat : {k+1}')
+
     frames = []
 
     current_frame = np.zeros((N, N), dtype=int)
@@ -90,7 +128,7 @@ def simulate_single_repeat(k, sfx1, N, max_steps, base_mating_success_rate_donor
     for step in range(max_steps):
         intermediate_frame_donor1 = np.zeros((N, N), dtype=int)
 
-        update_frames(N, current_frame, intermediate_frame_donor1, sfx1, base_mating_success_rate_donor1)
+        update_frames(N, current_frame, intermediate_frame_donor1, EI1, base_mating_success_rate_donor1)
         next_frame = apply_updates(N, current_frame, intermediate_frame_donor1, dead_cutoff)
 
         current_frame = np.copy(next_frame)
@@ -114,163 +152,98 @@ def simulate_single_repeat(k, sfx1, N, max_steps, base_mating_success_rate_donor
 
     return frames_results
 
-def main_loop(repeats=1, sfx1=1, N=100, max_steps=500, base_mating_success_rate_donor1=0.8, return_full_movie=False, dead_cutoff=DEAD_CUTOFF) -> list:
+def main_loop(repeats=1, EI1=1, N=100, max_steps=500, base_mating_success_rate_donor1=0.8, return_full_movie=False, dead_cutoff=DEAD_CUTOFF, print_repeat=False) -> list:
     all_repeats = []
     for k in range(repeats) :
-        results = simulate_single_repeat(k=k, sfx1=sfx1, N=N, max_steps=max_steps, base_mating_success_rate_donor1=base_mating_success_rate_donor1, return_full_movie=return_full_movie, dead_cutoff=dead_cutoff)
+        results = simulate_single_repeat(k=k, EI1=EI1, N=N, max_steps=max_steps, base_mating_success_rate_donor1=base_mating_success_rate_donor1, return_full_movie=return_full_movie, dead_cutoff=dead_cutoff, print_repeat=print_repeat)
         all_repeats.append(results)
     return all_repeats
 
 
+if args.mode in ('batch', 'b'):
+    start = time.time()
+    # MANUAL PARAMETERS
+    EI_possible_values = args.exclusion_indices
+    N = args.frame_size #length and width of the canvas
+    repeats = args.repeat
+    max_steps = args.sim_len
+    num_channels = 2 # Types of data to plot (plasmid 1, plasmid 2, dead percentages)
+    alpha=1 #opacity of lines for plots
+
+    all_results = np.empty([len(EI_possible_values), num_channels, max_steps, repeats], dtype=np.float32)
+    sim_lengths = np.zeros([len(EI_possible_values), repeats])
+
+    # EI_table[k, l, :] = combinations of EI values
+    for i, EI in enumerate(EI_possible_values):
+        all_repeats = main_loop(repeats=repeats, EI1=EI, N=N, max_steps=max_steps, dead_cutoff=DEAD_CUTOFF, return_full_movie=False, print_repeat=True)
+        for r, frames in enumerate(all_repeats):
+            all_results[i, 0, :, r] = frames[:, 0]
+            all_results[i, 1, :, r] = frames[:, 1]
+
+    print(f'Time elapsed : {time.time() - start} s')
+
+    with open('one_donor_all_results_250.pkl', 'wb') as file:
+        pkl.dump(all_results, file)
+
+    with open('one_donor_all_results_250.pkl', 'rb') as file:
+        all_results = pkl.load(file)
+
+    # Data for bar plots
+    categories = ['Plasmid 1', 'Dead']
+    colors = ['#1f77b4', '#3BA941', '#FFAA3B']
+
+    fig, axs = plt.subplots(1, 2, figsize=(9, 4), dpi=500)
+    plt.subplots_adjust(bottom=0.25)
+    axs = axs.flatten()
+
+    def plot_prct(ax, prct, label, color):
+        prct_mean = prct.mean(axis=1)
+        prct_std = prct.std(axis=1)
+
+        ci_upper = prct_mean + prct_std
+        ci_lower = prct_mean - prct_std
+
+        x = list(range(len(prct_mean)))
+
+        ax.plot(x, prct_mean, label=label, color=color, alpha=1, linewidth=3)
+        ax.fill_between(x=x, y1=ci_upper, y2=ci_lower, alpha=0.2, color=color)
 
 
+    # Create each bar plot in the grid
 
+    axs[0].set_title(f'Proportion of donors', fontsize=16)
+    axs[1].set_title(f'Proportion of dead cells', fontsize=16)
 
+    for i, EI in enumerate(EI_possible_values):
+        color = colors[i]
+        sim_len = max_steps
+        plot_prct(axs[0], all_results[i, 0, :, :], f'EI = {EI}', color)
+        plot_prct(axs[1], all_results[i, 1, :, :], f'EI = {EI}', color)
 
+    for k, ax in enumerate(axs):
+        ax.set_ylim(-0.1, 1.1)
+        ax.set_xlim(0, sim_len)
+        ax.tick_params(axis='both', which='major', labelsize=15)
+        ax.set_xlabel('Frame', fontsize=16)
 
-#
-#
-# start = time.time()
-# mpl.rcParams['figure.dpi'] = 500
-# # MANUAL PARAMETERS
-# sfx_possible_values = [1, 5, 200]
-# N = 100 #length and width of the canvas
-# repeats = 250
-# max_steps = 500
-# num_channels = 2 # Types of data to plot (plasmid 1, plasmid 2, dead percentages)
-# alpha=1 #opacity of lines for plots
-#
-# all_results = np.empty([len(sfx_possible_values), num_channels, max_steps, repeats], dtype=np.float32)
-# sim_lengths = np.zeros([len(sfx_possible_values), repeats])
-#
-# # sfx_table[k, l, :] = combinations of sfx values
-# for i, sfx in enumerate(sfx_possible_values):
-#     all_repeats = main_loop(repeats=repeats, sfx1=sfx, N=N, max_steps=max_steps, dead_cutoff=DEAD_CUTOFF, return_full_movie=False)
-#     for r, frames in enumerate(all_repeats):
-#         all_results[i, 0, :, r] = frames[:, 0]
-#         all_results[i, 1, :, r] = frames[:, 1]
-#
-# print(f'Time elapsed : {time.time() - start} s')
-#
-# with open('one_donor_all_results_250.pkl', 'wb') as file:
-#     pkl.dump(all_results, file)
-#
-# with open('one_donor_all_results_250.pkl', 'rb') as file:
-#     all_results = pkl.load(file)
-#
-# # Data for bar plots
-# categories = ['Plasmid 1', 'Dead']
-# colors = ['#1f77b4', '#3BA941', '#FFAA3B']
-#
-# fig, axs = plt.subplots(1, 2, figsize=(9, 4))
-# plt.subplots_adjust(bottom=0.25)
-# axs = axs.flatten()
-#
-# def plot_prct(ax, prct, label, color):
-#     prct_mean = prct.mean(axis=1)
-#     prct_std = prct.std(axis=1)
-#
-#     ci_upper = prct_mean + prct_std
-#     ci_lower = prct_mean - prct_std
-#
-#     x = list(range(len(prct_mean)))
-#
-#     ax.plot(x, prct_mean, label=label, color=color, alpha=1, linewidth=3)
-#     ax.fill_between(x=x, y1=ci_upper, y2=ci_lower, alpha=0.2, color=color)
-#
-#
-# # Create each bar plot in the grid
-#
-# axs[0].set_title(f'Proportion of donors', fontsize=16)
-# axs[1].set_title(f'Proportion of dead cells', fontsize=16)
-#
-# for i, sfx in enumerate(sfx_possible_values):
-#     color = colors[i]
-#     sim_len = max_steps
-#     plot_prct(axs[0], all_results[i, 0, :, :], 'Plasmid 1', color)
-#     plot_prct(axs[1], all_results[i, 1, :, :], 'Dead', color)
-#
-# for k, ax in enumerate(axs):
-#     ax.set_ylim(-0.1, 1.1)
-#     ax.set_xlim(0, sim_len)
-#     ax.tick_params(axis='both', which='major', labelsize=15)
-#     ax.set_xlabel('Frame', fontsize=16)
-#
-#     if k % 3 != 0:  # Subplots not on the leftmost column
-#         ax.set_yticks([])
-#     else:
-#         ax.set_ylabel('Proportion', fontsize=16)
-#
-# # Adjust layout to prevent overlap
-# plt.tight_layout()
-#
-# # Display the plot
-# # plt.show()
-# plt.savefig('one_donor_all_results_250_merged.svg')
-#
-# print('Done')
+        if k % 3 != 0:  # Subplots not on the leftmost column
+            ax.set_yticks([])
+        else:
+            ax.set_ylabel('Proportion', fontsize=16)
 
+    plt.legend(loc='lower right', bbox_to_anchor=(1, 0., 0.5, 0.5), fontsize=12)
 
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
 
+    if args.output == '':
+        plt.show()
+    else :
+        plt.savefig(f'one_donor_batch_{repeats}.svg')
 
+    print('Done')
 
-
-
-
-# if plot_animation :
-#     from matplotlib.colors import LinearSegmentedColormap, Normalize
-#     # Define custom colormap values and colors
-#     cmap_values = [0, 1, 100]  # Define the values at which the colors change
-#     cmap_colors = ['white', 'tab:blue', 'black']  # Corresponding colors for the values
-#     cmap_name = 'custom_cmap'
-#
-#     # Normalize values to be between 0 and 1
-#     norm = Normalize(vmin=min(cmap_values), vmax=max(cmap_values))
-#
-#     # Create the custom colormap
-#     colors = [(norm(val), color) for val, color in zip(cmap_values, cmap_colors)]
-#     custom_cmap = LinearSegmentedColormap.from_list(cmap_name, colors, N=256)
-#
-#     sfx=1
-#     frames = main_loop(N=100, sfx1=sfx, return_full_movie=True)[0]
-#
-#     # Set up the figure and plot elements
-#     fig, axs = plt.subplots(1, 3, figsize=(8, 6))
-#     plt.subplots_adjust(bottom=0.25)
-#
-#     plot_frames = [20, 50, 150]
-#     for i, frame_no in enumerate(plot_frames):
-#         axs[i].matshow(frames[frame_no], cmap=custom_cmap)
-#         axs[i].set_yticks([])
-#         axs[i].set_xticks([])
-#     # plt.title(f'Sfx = {sfx}')
-#
-#     # Create custom legend for defined colors
-#     legend_elements = [
-#         plt.Line2D([0], [0], marker='s', color='gray', markersize=10, label='Recipient', markerfacecolor='white', linestyle='None'),
-#         plt.Line2D([0], [0], marker='s', color='tab:blue', markersize=10, label='Donor', markerfacecolor='tab:blue', linestyle='None'),
-#         plt.Line2D([0], [0], marker='s', color='black', markersize=10, label='Dead', markerfacecolor='black', linestyle='None')
-#     ]
-#
-#     plt.legend(handles=legend_elements, loc='lower right', bbox_to_anchor=(1, 1))
-#
-#     # Display the plot with the slider and play/pause button
-#     # plt.show()
-#     plt.savefig('one_donor_animation.svg')
-
-
-
-
-
-
-
-
-
-
-
-
-
-if plot_animation :
+if args.mode in ('movie', 'm') :
     from matplotlib.widgets import Slider, Button
     import matplotlib.animation as animation
     from matplotlib.colors import LinearSegmentedColormap, Normalize
@@ -278,29 +251,26 @@ if plot_animation :
     mpl.use("TkAgg")
 
     # Define custom colormap values and colors
-    cmap_values = [0, 1, 100]  # Define the values at which the colors change
-    cmap_colors = ['white', 'tab:blue', 'black']  # Corresponding colors for the values
-    cmap_name = 'custom_cmap'
+    cmap_values = [0, 1, 100]
+    cmap_colors = ['white', 'tab:blue', 'black']
 
-    # Normalize values to be between 0 and 1
+    # Normalize values and to allow non-equal spacing between the cmap values
     norm = Normalize(vmin=min(cmap_values), vmax=max(cmap_values))
-
-    # Create the custom colormap
     colors = [(norm(val), color) for val, color in zip(cmap_values, cmap_colors)]
-    custom_cmap = LinearSegmentedColormap.from_list(cmap_name, colors, N=256)
+    custom_cmap = LinearSegmentedColormap.from_list('custom_cmap', colors, N=256)
 
-    sfx=20000
-    frames = main_loop(N=100, sfx1=sfx, return_full_movie=True)[0]
+    EI=args.exclusion_index
+    frames = main_loop(N=args.frame_size, EI1=EI, return_full_movie=True)[0]
     num_frames = len(frames)
 
     # Set up the figure and plot elements
     fig, ax = plt.subplots(figsize=(8, 6))
     plt.subplots_adjust(bottom=0.25)
-    cax = ax.matshow(frames[0], cmap=custom_cmap, vmin=0, vmax=100)
-    plt.title(f'Sfx = {sfx}')
+    cax = ax.matshow(frames[0], cmap=custom_cmap, vmin=min(cmap_values), vmax=max(cmap_values))
+    plt.title(f'EI = {EI}')
 
     # Add a slider for controlling the frame
-    ax_slider = plt.axes([0.25, 0.1, 0.5, 0.03], facecolor='lightgoldenrodyellow')
+    ax_slider = plt.axes([0.25, 0.1, 0.5, 0.03])
     slider = Slider(ax_slider, 'Frame', 0, num_frames - 1, valinit=0, valstep=1)
 
     # Add play/pause button
@@ -353,8 +323,54 @@ if plot_animation :
 
     plt.legend(handles=legend_elements, loc='lower right', bbox_to_anchor=(1.5, 5))
 
-    # Display the plot with the slider and play/pause button
-    plt.show()
+    if args.output == '' :
+        # Display the plot with the slider and play/pause button
+        plt.show()
+    else :
+        # Save animation as mp4
+        ani.save(f'TraT_sim_one_donor_EI{EI}_overmating{DEAD_CUTOFF}.mp4', writer='ffmpeg', fps=30)
 
-    # Save animation as mp4
-    # ani.save(f'TraT_sim_one_donor_sfx{sfx}_overmating{DEAD_CUTOFF}.mp4', writer='ffmpeg', fps=30)
+
+# # =============================
+# # Output movie snapshots, uncomment the section below to activate
+# # =============================
+#
+# from matplotlib.colors import LinearSegmentedColormap, Normalize
+# # Define custom colormap values and colors
+# cmap_values = [0, 1, 100]  # Define the values at which the colors change
+# cmap_colors = ['white', 'tab:blue', 'black']  # Corresponding colors for the values
+# cmap_name = 'custom_cmap'
+#
+# # Normalize values to be between 0 and 1
+# norm = Normalize(vmin=min(cmap_values), vmax=max(cmap_values))
+#
+# # Create the custom colormap
+# colors = [(norm(val), color) for val, color in zip(cmap_values, cmap_colors)]
+# custom_cmap = LinearSegmentedColormap.from_list(cmap_name, colors, N=256)
+#
+# EI=1
+# frames = main_loop(N=100, EI1=EI, return_full_movie=True)[0]
+#
+# # Set up the figure and plot elements
+# fig, axs = plt.subplots(1, 3, figsize=(8, 6))
+# plt.subplots_adjust(bottom=0.25)
+#
+# plot_frames = [20, 50, 150]
+# for i, frame_no in enumerate(plot_frames):
+#     axs[i].matshow(frames[frame_no], cmap=custom_cmap)
+#     axs[i].set_yticks([])
+#     axs[i].set_xticks([])
+# # plt.title(f'EI = {EI}')
+#
+# # Create custom legend for defined colors
+# legend_elements = [
+#     plt.Line2D([0], [0], marker='s', color='gray', markersize=10, label='Recipient', markerfacecolor='white', linestyle='None'),
+#     plt.Line2D([0], [0], marker='s', color='tab:blue', markersize=10, label='Donor', markerfacecolor='tab:blue', linestyle='None'),
+#     plt.Line2D([0], [0], marker='s', color='black', markersize=10, label='Dead', markerfacecolor='black', linestyle='None')
+# ]
+#
+# plt.legend(handles=legend_elements, loc='lower right', bbox_to_anchor=(1, 1))
+#
+# # Display the plot with the slider and play/pause button
+# plt.show()
+# # plt.savefig('one_donor_animation.svg')
